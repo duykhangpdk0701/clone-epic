f91ecb725a6eb32139c59606ad5d106064562447
Phùng Duy Khang <61218936+duykhangpdk0701@users.noreply.github.com>
2021-10-13 21:01:58 +0700

    modifile logic url sortBy

--- client/src/features/browse/listProduct/SortListProduct.jsx
+++ client/src/features/browse/listProduct/SortListProduct.jsx
@@ -1,11 +1,13 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useHistory } from "react-router-dom";
 import queryString from "query-string";
 //import scss
 import style from "./SortListProduct.module.scss";
-import { ReplaceUrtUrl } from "../../../helper/RepalceSortUrl";
+import { ReplaceUrtUrl } from "../../../helper/ReplaceSortUrl";
+import * as SortType from "../../../helper/SortType";
 
 const SortListProduct = () => {
+  const [currentSort, setCurrentSort] = useState("New release");
   const history = useHistory();
   const urlParams = useMemo(
     () =>
@@ -16,35 +18,65 @@ const SortListProduct = () => {
     [history.location.search],
   );
 
+  useEffect(() => {
+    const sortByUrl = urlParams.sortBy;
+    switch (sortByUrl) {
+      case SortType.NEW_RELEASE.urlValue:
+        setCurrentSort(SortType.NEW_RELEASE.value);
+        break;
+      case SortType.RELEASE_DATE.urlValue:
+        setCurrentSort(SortType.RELEASE_DATE.value);
+        break;
+      case SortType.ALPHABETICAL.urlValue:
+        setCurrentSort(SortType.ALPHABETICAL.value);
+        break;
+      case SortType.LOW_TO_HIGH.urlValue:
+        setCurrentSort(SortType.LOW_TO_HIGH.value);
+        break;
+      case SortType.HIGH_TO_LOW.urlValue:
+        setCurrentSort(SortType.HIGH_TO_LOW.value);
+        break;
+      default:
+        break;
+    }
+  });
+
   const handleSortNewRelease = () => {
-    ReplaceUrtUrl("NewRelease", history, urlParams);
+    ReplaceUrtUrl(SortType.NEW_RELEASE.urlValue, history, urlParams);
+    setCurrentSort(SortType.NEW_RELEASE.urlValue);
   };
   const handleSortReleaseDate = () => {
-    ReplaceUrtUrl("ReleaseDate", history, urlParams);
+    ReplaceUrtUrl(SortType.RELEASE_DATE.urlValue, history, urlParams);
+    setCurrentSort(SortType.RELEASE_DATE.urlValue);
   };
   const handleSortAlphabetical = () => {
-    ReplaceUrtUrl("Alphabetical", history, urlParams);
+    ReplaceUrtUrl(SortType.ALPHABETICAL.urlValue, history, urlParams);
+    setCurrentSort(SortType.ALPHABETICAL.urlValue);
   };
   const handleSortLowToHigh = () => {
-    ReplaceUrtUrl("LowToHigh", history, urlParams);
+    ReplaceUrtUrl(SortType.LOW_TO_HIGH.urlValue, history, urlParams);
+    setCurrentSort(SortType.LOW_TO_HIGH.urlValue);
   };
   const handleSortHighToLow = () => {
-    ReplaceUrtUrl("HighToLow", history, urlParams);
+    ReplaceUrtUrl(SortType.HIGH_TO_LOW.urlValue, history, urlParams);
+    setCurrentSort(SortType.HIGH_TO_LOW.urlValue);
   };
 
   return (
-    <div className={style.drop_down}>
-      <button>
-        <span>Sort By: </span>
-        <span>New release</span>
-        <span></span>
-      </button>
-      <div>
-        <button onClick={handleSortNewRelease}>New release</button>
-        <button onClick={handleSortReleaseDate}>Release date</button>
-        <button onClick={handleSortAlphabetical}>Alphabetical</button>
-        <button onClick={handleSortLowToHigh}>Price: Low to High</button>
-        <button onClick={handleSortHighToLow}>Price: Hight to Low</button>
+    <div className={style.side_bar}>
+      <div className={style.drop_down}>
+        <button className={style}>
+          <span>Sort By: </span>
+          <span>{currentSort}</span>
+          <span></span>
+        </button>
+        <div>
+          <button onClick={handleSortNewRelease}>New release</button>
+          <button onClick={handleSortReleaseDate}>Release date</button>
+          <button onClick={handleSortAlphabetical}>Alphabetical</button>
+          <button onClick={handleSortLowToHigh}>Price: Low to High</button>
+          <button onClick={handleSortHighToLow}>Price: Hight to Low</button>
+        </div>
       </div>
     </div>
   );
---
+++ client/src/helper/SortType.js
@@ -0,0 +1,24 @@
+export const NEW_RELEASE = {
+  value: "New release",
+  urlValue: "NewRelease",
+};
+
+export const RELEASE_DATE = {
+  value: "Release date",
+  urlValue: "ReleaseDate",
+};
+
+export const ALPHABETICAL = {
+  value: "Alphabetical",
+  urlValue: "Alphabetical",
+};
+
+export const LOW_TO_HIGH = {
+  value: "Price: Low to High",
+  urlValue: "LowToHigh",
+};
+
+export const HIGH_TO_LOW = {
+  value: "Price: Hight to Low",
+  urlValue: "HighToLow",
+};
